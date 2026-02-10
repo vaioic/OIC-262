fe53a912bf2bef6c14ebab1a062e01c5aa66888e
Jian Tay <jian.tay@vai.org>
2026-02-10 17:29:44 -0500

    Working analysis code

--- dev_analyze_color.py
+++ dev_analyze_color.py
@@ -5,67 +5,75 @@ from matplotlib import colors as mcolors
 import skimage
 import numpy as np
 import sklearn
+from pathlib import Path
 
-# image_file = '..\\data\\WT-300\\P5-Image.tiff'
-# mask_file = '..\\data\\WT-300\\qupath\\export\\P5-Image-labels.png'
+mask_file = Path('..\\newdata\\isp1\\qupath\\export\\isp1_s1c0x24942-4096y0-3008m0-labels.png')
+image_directory = Path('..\\newdata\\isp1\\isp1.tiff_files')
 
-image_file = '..\\data\\isp1\\P14-Image.tiff'
-mask_file = '..\\data\\isp1\\qupath\\export\\P14-Image-labels.png'
+output_directory = Path('..\\processed\\2026-02-10')
 
+# Begin processing code
+image_file = image_directory / (mask_file.stem[:-7] + '.tiff')
+
+# Read in images
 image = skimage.io.imread(image_file)
+print(image.dtype)
 mask = skimage.io.imread(mask_file)
 
-# Should I be doing this or does this mess up the original image?
-# image = (image - np.min(image)) / (np.max(image) - np.min(image))
+# Get cells
+cell_labels, nLabels = skimage.measure.label(mask == 1, return_num=True)
 
-# plt.imshow(image)
-# plt.show()
-
-# Note: Mask is in RGB
-mask_gray = skimage.color.rgb2gray(mask)
-mask_gray = skimage.util.img_as_ubyte(mask_gray)
-# plt.imshow(mask_gray)
-# plt.show()
-
-#Get values: [ 0 91 171 193]
-unique = np.unique(mask_gray)
-unique = np.delete(unique, np.where(unique == 0))
-
+# Convert image to HSV color space for color analysis
 image_hsv = skimage.color.rgb2hsv(image)
 
+output_directory.mkdir(exist_ok=True)
+
 # Quantify the HSV in each mask
-for obj_id in unique:
+for obj_id in range(nLabels):
 
-    hue_channel = (image_hsv[..., 0]).squeeze()
-    sat_channel = (image_hsv[..., 1]).squeeze()
-    value_channel = (image_hsv[..., 2]).squeeze()
+    # Get the HSV values for current cell. Data is N x 3 where N is the pixel
+    hsv_values = image_hsv[cell_labels == (obj_id + 1)]
+    # print(hsv_values.shape)
 
-    # Calculate average of each metric
-    mean_hue = np.mean(hue_channel[np.where(mask_gray == obj_id)])
+    # Calculate the average H, S, V
+    mean_HSV = np.mean(hsv_values, axis=0)
+    # print(mean_HSV)
 
-    print(mean_hue)
-
-    # KMeans expects each row to be a pixel and the coluns to be H, S, V
-    X = image_hsv.reshape((-1, 3))
-    
-    # Try k-means clustering
+    # Calculate k-means clustering. Input should be N x 3.
     kmeans = sklearn.cluster.KMeans(n_clusters=2, n_init="auto")
-    labels = kmeans.fit_predict(X)
+    labels = kmeans.fit_predict(hsv_values)
     centers = kmeans.cluster_centers_
 
     print(f"k-means centers: {centers}")
 
-    fig = plt.figure(figsize=(10, 8))
+    fig = plt.figure(figsize=(12, 10))
     gs = gridspec.GridSpec(2, 3, figure=fig)
+
+    obj_mask = cell_labels == (obj_id + 1)
+    outline = skimage.segmentation.find_boundaries(obj_mask, mode='thick')
+    outline = skimage.morphology.dilation(outline, skimage.morphology.disk(4))
+
+    image_out = image.copy()
+
+    image_out_r = image_out[:, :, 0]
+    image_out_g = image_out[:, :, 1]
+    image_out_b = image_out[:, :, 2]
+
+    image_out_r[outline] = 0
+    image_out_g[outline] = 255
+    image_out_b[outline] = 0
+
+    ax0 = fig.add_subplot(gs[0, 0])
+    ax0.imshow(image_out)
     
-    ax1 = fig.add_subplot(gs[0, :], projection='3d')
+    ax1 = fig.add_subplot(gs[0, 1:2], projection='3d')
 
     # Pick a random selection of points
     rng = np.random.default_rng()
-    pts = rng.integers(low=0, high=X.shape[0], size=200)
+    pts = rng.integers(low=0, high=hsv_values.shape[0], size=200)
 
     # Scatter plot of the data points, colored by their assigned cluster label
-    ax1.scatter(X[pts, 0], X[pts, 1], X[pts, 2], c=labels[pts], cmap='viridis', s=50, alpha=0.6)
+    ax1.scatter(hsv_values[pts, 0], hsv_values[pts, 1], hsv_values[pts, 2], c=labels[pts], cmap='viridis', s=50, alpha=0.6)
 
     # Plot the centroids as well
     ax1.scatter(centers[:, 0], centers[:, 1], centers[:, 2], marker='X', s=200, c='red', label='Centroids')
@@ -74,37 +82,37 @@ for obj_id in unique:
     ax1.set_xlabel('Hue')
     ax1.set_ylabel('Saturation')
     ax1.set_zlabel('Value/Brightness')
-    ax1.legend()
+    #ax1.legend()
 
     # Make an inset that shows rectangles of the centroid colors
-    rgb_color1 = mcolors.hsv_to_rgb(centers[0, :])
-    rgb_color2 = mcolors.hsv_to_rgb(centers[1, :])
+    rgb_color1 = skimage.color.hsv2rgb(centers[0, :])
+    rgb_color2 = skimage.color.hsv2rgb(centers[1, :])
     
-    inset1 = ax1.inset_axes([0.65, 0.65, 0.3, 0.3]) 
-    box1 = mpatches.Rectangle((0.1, 0.1), 0.35, 0.8, color=rgb_color1, transform=inset1.transAxes)
-    box2 = mpatches.Rectangle((0.55, 0.1), 0.35, 0.8, color=rgb_color2, transform=inset1.transAxes)
-
-    inset1.add_patch(box1)
+    inset1 = ax1.inset_axes([1.5, 0.5, 0.3, 0.3]) 
+    box1 = mpatches.Rectangle((0.1, 0.1), 0.35, 0.35, color=rgb_color1, transform=inset1.transAxes)
+    
+    inset1.add_patch(box1)   
+    
+    inset1.text(0.275, 0.0, f'HSV={np.round(centers[0, :], 2)}', ha='center', va='top', transform=inset1.transAxes, fontsize=8)
+    
+    box2 = mpatches.Rectangle((0.1, 0.70), 0.35, 0.35, color=rgb_color2, transform=inset1.transAxes)
     inset1.add_patch(box2)
-    inset1.text(0.275, 0.05, f'HSV={centers[0,:]}', ha='center', va='top', transform=inset1.transAxes, fontsize=8)
-    inset1.text(0.725, 0.05, f'HSV={centers[1,:]}', ha='center', va='top',  transform=inset1.transAxes, fontsize=8)
-
+    inset1.text(0.275, 0.655, f'HSV={np.round(centers[1, :], 2)}', ha='center', va='top',  transform=inset1.transAxes, fontsize=8)
 
     inset1.set_xlim(0, 1)
     inset1.set_ylim(0, 1)
     inset1.axis('off') # Hide the axes ticks and spines
 
     ax2 = fig.add_subplot(gs[1, 0])
-    ax2.scatter(X[pts, 0], X[pts, 1], c=labels[pts], cmap='viridis')
+    ax2.scatter(hsv_values[pts, 0], hsv_values[pts, 1], c=labels[pts], cmap='viridis')
     ax2.scatter(centers[:, 0], centers[:, 1], marker='X', c='red')
     ax2.set_xlim(0, 1)
     ax2.set_ylim(0, 1)
     ax2.set_xlabel('Hue')
     ax2.set_ylabel('Saturation')
 
-
     ax3 = fig.add_subplot(gs[1, 1])
-    ax3.scatter(X[pts, 0], X[pts, 2], c=labels[pts], cmap='viridis')
+    ax3.scatter(hsv_values[pts, 0], hsv_values[pts, 2], c=labels[pts], cmap='viridis')
     ax3.scatter(centers[:, 0], centers[:, 2], marker='X', c='red')
     ax3.set_xlim(0, 1)
     ax3.set_ylim(0, 1)
@@ -112,13 +120,12 @@ for obj_id in unique:
     ax3.set_ylabel('Value')
 
     ax4 = fig.add_subplot(gs[1, 2])
-    ax4.scatter(X[pts, 1], X[pts, 2], c=labels[pts], cmap='viridis')
+    ax4.scatter(hsv_values[pts, 1], hsv_values[pts, 2], c=labels[pts], cmap='viridis')
     ax4.scatter(centers[:, 1], centers[:, 2], marker='X', c='red')
     ax4.set_xlim(0, 1)
     ax4.set_ylim(0, 1)
     ax4.set_xlabel('Saturation')
     ax4.set_ylabel('Value')
 
-    plt.show()
-
-    exit()
+    fig.savefig(output_directory / (mask_file.stem[:-7] + "_cell" + f"{obj_id + 1}" + ".jpg"))
+    #plt.show()
